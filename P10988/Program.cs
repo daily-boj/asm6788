7ac2ef7c8132ad10d1e013841422d2b5628ce57a
asm6788 <asm6788@naver.com>
2020-04-30 08:55:04 +0900

    10988

---
+++ P10988/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            if(input == new string(charArray))
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+        }
+    }
+}
